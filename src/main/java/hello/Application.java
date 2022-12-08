db105f6c478546abb29f89e21a51f019603d451d
Jason Liu <jasonliu@example.com>
2022-12-08 08:32:50 +0000

    update to v3

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello World v2.\n";
+    return "Hello World v3.\n";
   }
 
   public static void main(String[] args) {
