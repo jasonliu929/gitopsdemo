8d9dc8960c80d95bb1a0c49eefac0e7eef4b0160
Jason Liu <jasonliu@example.com>
2022-12-08 06:06:12 +0000

    change to version 2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello World v1.\n";
+    return "Hello World v2.\n";
   }
 
   public static void main(String[] args) {
